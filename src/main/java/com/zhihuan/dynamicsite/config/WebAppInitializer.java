f7966ed238b79c26d7790128c0d160f5b428062e
luoweiqiang <763231564@qq.com>
2018-04-08 15:12:09 +0800

    集成SiteMesh3

--- src/main/java/com/zhihuan/dynamicsite/config/WebAppInitializer.java
+++ src/main/java/com/zhihuan/dynamicsite/config/WebAppInitializer.java
@@ -1,6 +1,9 @@
 package com.zhihuan.dynamicsite.config;
 
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     protected Class<?>[] getRootConfigClasses() {
@@ -13,5 +16,11 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        ConfigurableSiteMeshFilter siteMeshFilter=new ConfigurableSiteMeshFilter();
+        return new Filter[]{siteMeshFilter};
     }
 }
---
+++ src/main/webapp/WEB-INF/decorator/decorator.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: luoweiqiang
+  Date: 2018/4/8
+  Time: 15:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title><sitemesh:write property='title'/>--SiteMesh</title>
+    <sitemesh:write property='head'/>
+</head>
+<body>
+    <sitemesh:write property='body'/>
+</body>
+</html>
